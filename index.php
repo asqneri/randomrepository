56f29231227dda698e8623119e374fd9e12a7ecf
asqneri <wirvorfi@gmail.com>
2025-11-06 13:14:40 +0300

    Доавили функцию ввода чисел

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-function printMenu() {
-    echo "1. Ввести два числа\n";
-    echo "2. Сложение\n";
-    echo "3. Вычитание\n";
-    echo "4. Деление\n";
-    echo "5. Возведение в степень\n";
-    echo "0. Выход\n";
+function input_numbers(&$a, &$b) {
+    $a = readline("Введите первое число: ");
+    $b = readline("Введите второе число: ");
 }
-
-$a = 0;
-$b = 0;
 ?>
